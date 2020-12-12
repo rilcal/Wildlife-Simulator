310bf9517243ba84c81435d1a69dbd81f42e82f8
Riley <rileycallaghan96@gmail.com>
2020-12-11 21:58:19 -0500

    Update A-Star.go
    Complete rough A* algo

--- pathfinding/A-Star.go
+++ pathfinding/A-Star.go
@@ -1,75 +1,111 @@
 package pathfinding
 
-import "github.com/rilcal/Wildlife-Simulator/structs"
+import (
+	"math"
+
+	"github.com/rilcal/Wildlife-Simulator/structs"
+)
 
 // Astar follows the a* search algorithm to quickly generate the fastest path to a given target point from a given start point in a matrix of given movement matrix
 func Astar(start structs.Point, target structs.Point, matrix [][]int) (path []structs.Point) {
-	openList := make([]structs.Point, 0, 50)
-	openList = append(openList, start)
-	//closedList := make([]structs.Point, 0, 50)
-	var current node
-	current.pos = start
 	xlen := len(matrix) - 1
 	ylen := len(matrix[1]) - 1
 
-	for i := 1; i > 0; {
+	var current node
+	current.pos = start
+	current.g = 0
+	current.pathto = make([]structs.Point, 0, 25)
+
+	openList := make([]node, 0, xlen*ylen)
+	openList = append(openList, current)
+
+	for i := 1; i < xlen*ylen*10; i++ {
+		if current.pos == target {
+			path = current.pathto
+			return
+		}
 		j := 0
+
+		//Generate neighbors slice
 		neighbors := make([]node, 0, 8)
 		//top left (-1, -1)
 		if (current.pos.X-1) < xlen && (current.pos.X-1) >= 0 && (current.pos.Y-1) < ylen && (current.pos.Y-1) >= 0 {
 			p := structs.NewPoint(current.parentPos.X-1, current.pos.Y-1)
-			neighbors[j] = newNode(p, current.pos)
+			g := current.g + matrix[current.parentPos.X-1][current.pos.Y-1]
+			neighbors[j] = newNode(p, current.pos, g, append(current.pathto, p))
 			j++
 		}
 
 		//top mid (0, -1)
 		if (current.pos.X) < xlen && (current.pos.X) >= 0 && (current.pos.Y-1) < ylen && (current.pos.Y-1) >= 0 {
 			p := structs.NewPoint(current.parentPos.X, current.pos.Y-1)
-			neighbors[j] = newNode(p, current.pos)
+			g := current.g + matrix[current.parentPos.X][current.pos.Y-1]
+			neighbors[j] = newNode(p, current.pos, g, append(current.pathto, p))
 			j++
 		}
 
 		//top right (+1, -1)
 		if (current.pos.X+1) < xlen && (current.pos.X+1) >= 0 && (current.pos.Y-1) < ylen && (current.pos.Y-1) >= 0 {
 			p := structs.NewPoint(current.parentPos.X+1, current.pos.Y-1)
-			neighbors[j] = newNode(p, current.pos)
+			g := current.g + matrix[current.parentPos.X+1][current.pos.Y-1]
+			neighbors[j] = newNode(p, current.pos, g, append(current.pathto, p))
 			j++
 		}
 
 		//mid left (-1, 0)
 		if (current.pos.X-1) < xlen && (current.pos.X-1) >= 0 && (current.pos.Y) < ylen && (current.pos.Y) >= 0 {
 			p := structs.NewPoint(current.parentPos.X-1, current.pos.Y)
-			neighbors[j] = newNode(p, current.pos)
+			g := current.g + matrix[current.parentPos.X-1][current.pos.Y]
+			neighbors[j] = newNode(p, current.pos, g, append(current.pathto, p))
 			j++
 		}
 
 		//mid right (+1, 0)
 		if (current.pos.X+1) < xlen && (current.pos.X+1) >= 0 && (current.pos.Y) < ylen && (current.pos.Y) >= 0 {
 			p := structs.NewPoint(current.parentPos.X+1, current.pos.Y)
-			neighbors[j] = newNode(p, current.pos)
+			g := current.g + matrix[current.parentPos.X+1][current.pos.Y]
+			neighbors[j] = newNode(p, current.pos, g, append(current.pathto, p))
 			j++
 		}
 
 		//botm left (-1, +1)
 		if (current.pos.X-1) < xlen && (current.pos.X-1) >= 0 && (current.pos.Y+1) < ylen && (current.pos.Y+1) >= 0 {
 			p := structs.NewPoint(current.parentPos.X-1, current.pos.Y+1)
-			neighbors[j] = newNode(p, current.pos)
+			g := current.g + matrix[current.parentPos.X-1][current.pos.Y+1]
+			neighbors[j] = newNode(p, current.pos, g, append(current.pathto, p))
 			j++
 		}
 
 		//botm mid (0, +1)
 		if (current.pos.X) < xlen && (current.pos.X) >= 0 && (current.pos.Y+1) < ylen && (current.pos.Y+1) >= 0 {
 			p := structs.NewPoint(current.parentPos.X, current.pos.Y+1)
-			neighbors[j] = newNode(p, current.pos)
+			g := current.g + matrix[current.parentPos.X][current.pos.Y+1]
+			neighbors[j] = newNode(p, current.pos, g, append(current.pathto, p))
 			j++
 		}
 
 		//botm right (+1, +1)
 		if (current.pos.X+1) < xlen && (current.pos.X+1) >= 0 && (current.pos.Y+1) < ylen && (current.pos.Y+1) >= 0 {
 			p := structs.NewPoint(current.parentPos.X+1, current.pos.Y+1)
-			neighbors[j] = newNode(p, current.pos)
+			g := current.g + matrix[current.parentPos.X+1][current.pos.Y+1]
+			neighbors[j] = newNode(p, current.pos, g, append(current.pathto, p))
 			j++
 		}
+
+		//Loop through neighbors, calculate f score, and add to open list
+		for i := range neighbors {
+			openList = append(openList, neighbors[i])
+			h := math.Min(math.Abs(float64(neighbors[i].pos.X-target.X)), math.Abs(float64(neighbors[i].pos.Y-target.Y)))
+			neighbors[i].h = int(h)
+			neighbors[i].f = neighbors[i].g + neighbors[i].h
+		}
+
+		//remove current from open list
+		_ = findAndRemoveElement(current, &openList)
+
+		//find min
+		_, minInd := findMinF(&openList)
+		current = openList[minInd]
 	}
 	return
 }
@@ -77,11 +113,17 @@ func Astar(start structs.Point, target structs.Point, matrix [][]int) (path []st
 type node struct {
 	pos       structs.Point
 	parentPos structs.Point
+	pathto    []structs.Point
+	g         int
+	h         int
+	f         int
 }
 
-func newNode(pos, par structs.Point) (n node) {
+func newNode(pos structs.Point, par structs.Point, g int, parPath []structs.Point) (n node) {
 	n.pos = pos
 	n.parentPos = par
+	n.g = g
+	n.pathto = parPath
 	return
 }
 
@@ -95,5 +137,55 @@ func isIn(n node, slice []structs.Point) (b bool, ind int) {
 	}
 	b = false
 	ind = 0
+	return
+}
+
+func findMinFNode(l []node) (n node, ind int) {
+	minF := math.Inf(1)
+	ind = 0
+	for i := range l {
+		if float64(l[i].f) < minF {
+			minF = float64(l[i].f)
+			ind = i
+		}
+	}
+	n = l[ind]
+	return
+}
+
+func findElement(e node, l *[]node) (b bool, ind int) {
+	for i := range *l {
+		if e.pos == (*l)[i].pos {
+			b = true
+			ind = i
+			return
+		}
+	}
+	b = false
+	ind = 0
+	return
+}
+
+func findAndRemoveElement(e node, l *[]node) (b bool) {
+	found, index := findElement(e, l)
+	if found {
+		(*l)[index] = (*l)[len(*l)-1]
+		(*l) = (*l)[:len(*l)-1]
+		b = true
+		return
+	}
+	b = false
+	return
+}
+
+func findMinF(l *[]node) (minF int, ind int) {
+	minF = (*l)[0].f
+	ind = 0
+	for i := range *l {
+		if (*l)[i].f < minF {
+			minF = (*l)[i].f
+			ind = i
+		}
+	}
 	return
 }
